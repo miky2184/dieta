c52c72fd2d8053e53614b6dea8a62fc3b0674f39
Michele Micunco <michele.micunco@accenture.com>
2024-08-27 21:38:22 +0200

    fix

--- app/static/js/script.js
+++ app/static/js/script.js
@@ -1175,13 +1175,13 @@ function renderMenuEditor(data) {
             const mealTableHead = document.createElement('thead');
             mealTableHead.innerHTML = `
                 <tr>
-                    <th style="width:40%" class="text-align-center">${formatMealName(meal)}</th>
-                    <th style="width:10%" class="text-align-center">Kcal</th>
-                    <th style="width:10%" class="text-align-center">Carboidrati (g)</th>
-                    <th style="width:10%" class="text-align-center">Proteine (g)</th>
-                    <th style="width:10%" class="text-align-center">Grassi (g)</th>
-                    <th style="width:10%" class="text-align-center">Quantità</th>
-                    <th style="width:10%" class="text-align-center">Azioni</th>
+                    <th style="width:34%" class="text-align-center">${formatMealName(meal)}</th>
+                    <th style="width:11%" class="text-align-center">Kcal</th>
+                    <th style="width:11%" class="text-align-center">Carboidrati (g)</th>
+                    <th style="width:11%" class="text-align-center">Proteine (g)</th>
+                    <th style="width:11%" class="text-align-center">Grassi (g)</th>
+                    <th style="width:11%" class="text-align-center">Quantità</th>
+                    <th style="width:11%" class="text-align-center">Azioni</th>
                 </tr>
             `;
             mealTable.appendChild(mealTableHead);
