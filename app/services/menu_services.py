5bc9c7a8aad6ca43b9a676e082ef0edba0dd8c7b
Michele Micunco <michele.micunco@accenture.com>
2024-12-03 12:50:26 +0100

    fix

--- app/services/menu_services.py
+++ app/services/menu_services.py
@@ -451,7 +451,7 @@ def get_menu(user_id: int, period: dict = None, ids: int = None):
     result = query.first()
 
 
-    return {'menu': result.menu, 'data_fine': result.data_fine}
+    return result
 
 
 def get_settimane_salvate(user_id, show_old_week: bool = False):
