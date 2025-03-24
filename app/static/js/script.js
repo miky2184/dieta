29522c984c873ce711d01166b20195abb1f47a3b
Michele Micunco <michele.micunco@accenture.com>
2025-03-24 14:12:23 +0100

    fix consumi limiti

--- app/static/js/script.js
+++ app/static/js/script.js
@@ -1162,7 +1162,7 @@ function buildRemainingGruppiTable(menuConsumi, gruppi) {
     filteredGruppi.forEach(gruppo => {
         const id = gruppo.id.toString();
         const consumoRimanente = menuConsumi[id] || 0; // Prendi il consumo rimanente o 0 se non trovato
-        bodyHTML += `<td style="width:12% !important; text-align: center;">${consumoRimanente} g</td>`;
+        bodyHTML += `<td style="width:11% !important; text-align: center;">${consumoRimanente} g</td>`;
     });
     bodyHTML += '</tr>';
     remainingGruppiTableBody.innerHTML = bodyHTML;
