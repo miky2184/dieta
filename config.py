c5d1f8722964e8a4d3e23d0382858ef35840120d
Michele Micunco <michele.micunco@accenture.com>
2025-07-28 23:26:54 +0200

    migliotieW

--- config.py
+++ config.py
@@ -3,7 +3,6 @@ import os
 
 
 class Config:
-    LOG_LEVEL = os.getenv('LOG_LEVEL')
     SECRET_KEY = os.getenv('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = f"postgresql://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}@{os.getenv('DB_HOST')}:{os.getenv('DB_PORT')}/{os.getenv('DB_NAME')}"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
